919e41769df87b4b931e420855360401489bf06c
Mahmoud Said <mmsaiid@hotmail.com>
2021-02-15 11:23:56 +0200

    Creating Error

--- SUMU_Messenger.Website/Controllers/HomeController.cs
+++ SUMU_Messenger.Website/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace SUMU_Messenger.Website.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View()
         }
 
         public ActionResult About()
